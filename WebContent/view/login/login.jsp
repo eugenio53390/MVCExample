c9d5e9080de4e4985a2d11fc26436881fd8f05c2
GioIacca9 <giosue@iacca.ml>
2023-03-31 09:17:11 +0200

    Add login

---
+++ WebContent/view/login/login.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html class="h-100">
+<head>
+<title>Login</title>
+<%@include file="/view/structure/head.jsp" %>
+</head>
+<body class="h-100">
+<div class="container h-100">
+<main class="d-flex justify-content-center h-100 align-items-center">
+		<div class="card">
+			<div class="card-body p-4 p-md-5">
+				<form action="" method="post" data-bitwarden-watching="1">
+					<h1 class="text-center">Login</h1>
+
+					<div class="mb-3">
+						<label for="username" class="form-label">Username</label>
+						<input type="text" class="form-control" required autofocus name="username" id="username" placeholder="">
+					</div>
+
+					<div class="mb-3">
+						<label for="password" class="form-label">Password</label>
+						<input type="password" class="form-control" required name="password" id="password" placeholder="">
+					</div>
+					<p class="text-danger text-center">
+											</p>
+					<button class="btn btn-primary w-100">
+						Accedi
+					</button>
+				</form>
+			</div>
+		</div>
+	</main>
+</div>
+</body>
+</html>
---
+++ src/main/java/controller/Login.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends Session {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("view/login/login.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String password = request.getParameter("password");
+		String username = request.getParameter("username");
+		
+		// TODO: validate credentials
+	}
+
+}
