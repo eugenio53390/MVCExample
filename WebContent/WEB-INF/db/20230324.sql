3a44db3a5055e21c5f4af5de76cea9143e02fca4
stefano.darchivio <stefa@LAPTOP-N020TGGE>
2023-04-12 07:49:12 +0200

    Modificato script aggiunto in data 10/04/2023

--- WebContent/WEB-INF/db/20230324.sql
+++ WebContent/WEB-INF/db/20230324.sql
@@ -1,2 +1,1 @@
 ALTER TABLE `user` ADD `password` VARCHAR(25) NOT NULL AFTER `date_insert`;
-ALTER TABLE `user` ADD `role` VARCHAR(25) NOT NULL AFTER `password`;
---
+++ WebContent/WEB-INF/db/20230410.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `user` ADD `role` VARCHAR(25) NOT NULL AFTER `password`;
